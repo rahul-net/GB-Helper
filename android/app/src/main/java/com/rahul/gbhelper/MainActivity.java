a76e43c47150757c62f9d1847b9b8719924195b0
rahul-net <YOUR_GITHUB_EMAIL>
2026-06-15 21:44:46 +0400

    GBAPK project

---
+++ android/app/src/main/java/com/rahul/gbhelper/MainActivity.java
@@ -0,0 +1,5 @@
+package com.rahul.gbhelper;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rahul.gbhelper',
+  appName: 'GB Helper PRO',
+  webDir: 'www'
+};
+
+export default config;
